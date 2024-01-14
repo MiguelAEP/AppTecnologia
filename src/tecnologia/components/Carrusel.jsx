ecbc33d23551a3e4e3c7e302b3f32efd873e2ea2
= <miguelep24@hotmail.com>
2024-01-14 13:23:44 -0500

    mantenimiento

--- src/tecnologia/components/Carrusel.jsx
+++ src/tecnologia/components/Carrusel.jsx
@@ -11,13 +11,13 @@ export const Carrusel = ({ productos }) => {
 
                     </div>
                     <div className="carousel-item" data-bs-interval="1000">
-                        <img src={`/imagenes/${productos[15].thumbnail}.jpg`} style={{ height: '35vh' }} className="d-block w-100" />
+                        <img src={`/imagenes/${productos[1].thumbnail}.jpg`} style={{ height: '35vh' }} className="d-block w-100" />
                     </div>
                     <div className="carousel-item" data-bs-interval="1000">
-                        <img src={`/imagenes/${productos[19].thumbnail}.jpg`} style={{ height: '35vh' }} className="d-block w-100" />
+                        <img src={`/imagenes/${productos[2].thumbnail}.jpg`} style={{ height: '35vh' }} className="d-block w-100" />
                     </div>
                     <div className="carousel-item" data-bs-interval="1000">
-                        <img src={`/imagenes/${productos[25].thumbnail}.jpg`} style={{ height: '35vh' }} className="d-block w-100" />
+                        <img src={`/imagenes/${productos[3].thumbnail}.jpg`} style={{ height: '35vh' }} className="d-block w-100" />
                     </div>
                 </div>
             </div>
